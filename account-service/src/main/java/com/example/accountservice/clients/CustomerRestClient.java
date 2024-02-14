5bac6f42feecb2febc72a87b53f921b82e46ccc2
AbderrazzakNfissi <abderrazzak.nfissi@usmba.ac.ma>
2024-02-14 16:17:48 +0100

    add CircuitBreaker

--- account-service/src/main/java/com/example/accountservice/clients/CustomerRestClient.java
+++ account-service/src/main/java/com/example/accountservice/clients/CustomerRestClient.java
@@ -1,19 +1,26 @@
 package com.example.accountservice.clients;
-
 import com.example.accountservice.model.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.List;
 
 @FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerRestClient {
 
-    @GetMapping("/customers/{id}")
+    @GetMapping("customers/{id}")
+    @CircuitBreaker(name = "CustomerService",fallbackMethod = "getDefaultCustomer")
     Customer findCustomerById(@PathVariable  Long id);
-
-    @GetMapping("/customers")
+    @GetMapping("customers")
     List<Customer> findAllCustomers();
 
+    default Customer getDefaultCustomer(Long id,Exception exception){
+        Customer customer = new Customer();
+        customer.setId(0L);
+        customer.setEmail("default@gmail.com");
+        customer.setFirstName("DEFAULT_FN");
+        customer.setLastName("DEFAULT_LN");
+        return customer;
+    }
 }
